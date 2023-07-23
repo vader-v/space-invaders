2fb021c5816a6b2b14f1f27064acd0858a7c9215
amirb <amir.bullock9@gmail.com>
2023-07-23 19:11:43 -0400

    add functionality to drop grid by one row every time it bounces on the side of the screen

--- app.js
+++ app.js
@@ -118,11 +118,11 @@ class Invader {
       this.height
     )
   }
-  update() {
+  update({velocity}) {
   if (this.image){
     this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
+    this.position.x += velocity.x;
+    this.position.y += velocity.y;
   }
 }
 }
@@ -135,7 +135,7 @@ class Grid {
     }
 
     this.velocity = {
-      x: 0,
+      x: 2,
       y: 0
     }
 
@@ -143,6 +143,9 @@ class Grid {
 
     const columns = Math.floor(Math.random() * 10 + 5)
     const rows = Math.floor(Math.random() * 5 + 2)
+
+    this.width = columns * 30;
+    this.height = rows * 30;
     for (let x = 0; x < columns; x++) {
       for (let y = 0; y < rows; y++) {
       this.invaders.push(
@@ -156,8 +159,16 @@ class Grid {
       }
     }
   }
-  update() {
 
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.velocity.y = 0;
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 30;
+    }
   }
 }
 
@@ -194,7 +205,7 @@ function animate() {
   grids.forEach(grid => {
     grid.update();
       grid.invaders.forEach(invader => {
-        invader.update();
+        invader.update({velocity: grid.velocity});
       }
     )
   });
