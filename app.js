07180f983724b825f8d44421a2e01eb10f0e7621
amirb <amir.bullock9@gmail.com>
2023-07-24 20:17:00 -0400

    fix styling for canvas and body element relation to center canvas in window.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const scoreEl = document.querySelector('#scoreEl');
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 1024;
+canvas.height = 576;
 
 class Player {
     constructor() {
